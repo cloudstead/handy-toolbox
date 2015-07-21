8f415f53ba0ec0fc09a7bd139edb0fb40c9617bc
jonathan <jonathan@kyuss.org>
2015-07-21 16:10:01 -0400

    add libc6-i386, required for building windows app on linux with 32-bit java

--- bin/ubuntu_init.sh
+++ bin/ubuntu_init.sh
@@ -11,4 +11,4 @@ if [ $(whoami) != "root" ] ; then
   die "Must run as root"
 fi
 
-apt-get install -y pandoc zip wkhtmltopdf xorg xvfb hfsprogs
+apt-get install -y pandoc zip wkhtmltopdf xorg xvfb hfsprogs libc6-i386
